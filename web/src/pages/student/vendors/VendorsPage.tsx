fd71bb0a7beb36adb2d9b7b56483c8dd2f149e9b
S M Zawad Bin Zaki Sadaf <ssadaf@hawk.iit.edu>
2019-10-12 05:52:26 -0500

    Added Category to Whole Food Section of the page

--- web/src/pages/student/vendors/VendorsPage.tsx
+++ web/src/pages/student/vendors/VendorsPage.tsx
@@ -1,72 +1,41 @@
-import React from 'react';
-import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
-import IconButton from '@material-ui/core/IconButton';
-import StarBorderIcon from '@material-ui/icons/StarBorder';
+import React from "react";
+import { Theme, makeStyles, createStyles } from '@material-ui/core/styles';
+import ButtonBase from '@material-ui/core/ButtonBase';
+import Typography from '@material-ui/core/Typography';
+import { FancyRadioButtons } from "../../../components/fancy-radio-buttons/FancyRadioButtons";
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      display: 'flex',
-      flexWrap: 'wrap',
-      justifyContent: 'space-around',
-      overflow: 'hidden',
-      backgroundColor: theme.palette.background.paper,
-    },
-    gridList: {
-      flexWrap: 'nowrap',
-      // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
-      transform: 'translateZ(0)',
-    },
-    title: {
-      color: theme.palette.primary.light,
-    },
-    titleBar: {
-      background:
-        'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
-    },
-  }),
-);
-
-const tileData = [
+const images = [
   {
-    img: '/public/static/img/category-donor.png',
-    title: 'Image',
-    author: 'author',
+    url: '/static/img/BlueApron.jpg',
+    title: 'Blue Apron',
+    width: '50%',
   },
   {
-    img: '/public/static/img/category-donor.png',
-    title: 'Image',
-    author: 'author',
+    url: '/static/img/HelloFresh.jpg',
+    title: 'Hello Fresh',
+    width: '50%',
+  },
+  {
+    url: '/static/img/PurpleCarrot.jpg',
+    title: 'Purple Carrot',
+    width: '50%',
+  },
+  {
+    url: '/static/img/Freshly.jpg',
+    title: 'Freshly',
+    width: '50%',
+  },
+  {
+    url: '/static/img/SunBasket.jpg',
+    title: 'Sun Basket',
+    width: '50%',
   },
 ];
 
 export function VendorsPage() {
-  const classes = useStyles();
-
   return (
-    <div className={classes.root}>
-      <GridList className={classes.gridList} cols={2.5}>
-        {tileData.map(tile => (
-          <GridListTile key={tile.img}>
-            <img src={tile.img} alt={tile.title} />
-            <GridListTileBar
-              title={tile.title}
-              classes={{
-                root: classes.titleBar,
-                title: classes.title,
-              }}
-              actionIcon={
-                <IconButton aria-label={`star ${tile.title}`}>
-                  <StarBorderIcon className={classes.title} />
-                </IconButton>
-              }
-            />
-          </GridListTile>
-        ))}
-      </GridList>
-    </div>
-  );
+    <>
+      <FancyRadioButtons items={images} />
+    </>
+  )
 }
