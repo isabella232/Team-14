78308b72260612dbb14923699e50f1801273426d
Max Buffo <mbuffo@hawk.iit.edu>
2019-10-12 04:10:08 -0500

    docs: very rough draft of student dashboard page - more to come :)

--- web/src/pages/student/dashboard/Dashboard.tsx
+++ web/src/pages/student/dashboard/Dashboard.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
-
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 export function Dashboard() {
   return (
     <>
-      <p>THERE'S MORE TEXT THAT GOES HERE BRO</p>
-      <Button size="large">
-        Large
-      </Button>
-    </>
+      <p>Student Dashboard </p>
+      <Button size="large" color="primary" >
+        Order Groceries
+       </Button>
+    
+     <Button size="large" color="primary" >
+       Find Vendors Near Me
+     </Button>
+     <Paper>
+       Meals Redeemed To Date: xxx meals
+
+      </Paper>
+       Meal Balance: xxx meals
+
+
+</>
   );
 }
+
